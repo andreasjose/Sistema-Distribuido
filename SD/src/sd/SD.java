43ece065d231ccc86516c146a3c6587492105aef
andreasjose <42344930+andreasjose@users.noreply.github.com>
2019-11-25 19:40:07 -0300

    Add files via upload

---
+++ SD/src/sd/SD.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd;
+
+import org.netbeans.modules.java.j2seproject.copylibstask.*;
+import org.apache.tools.ant.*;
+import org.osgi.util.*;
+/**
+ *
+ * @author Andreas
+ */
+public class SD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CopyFiles texto = new CopyFiles();
+        
+    }
+    
+}
+
+//Link para baixar OSGI https://jar-download.com/artifacts/org.osgi
+//Link para baixar o apache-ant http://www.java2s.com/Code/Jar/a/Downloadapacheant182jar.htm
---
+++ SD/src/sd/teste.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd;
+
+import org.netbeans.modules.java.j2seproject.copylibstask.*;
+
+/**
+ *
+ * @author Andreas
+ */
+public class teste {
+    
+}
